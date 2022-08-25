4e6e504eb01fb6f1b65b8a2d3fb9e2a3cf4f6c7e
gjuancruz <tatugalaz@gmail.com>
2022-08-24 22:41:03 -0300

    framer motion animations

--- src/pages/Blue.jsx
+++ src/pages/Blue.jsx
@@ -2,33 +2,36 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import {motion} from 'framer-motion'
 import styles from './Blue.module.css'
+import { Box, Flex, Stack, Text, useMediaQuery } from "@chakra-ui/react"
 
 const Blue = () => {
+    const [isNotSmallerScreen] = useMediaQuery('(min-width:600px)')
+
         return (
             <motion.div 
-            className={styles.container}
-            intial={{width:0}}
-            animate={{width:"100%"}}
-            exit={{x:window.innerWidth,transition:{duration:0.1}}}
+            key={1}
+            initial={{y:-1000, opacity: 0}}
+            animate={{ y: 10, opacity: 1}}
+            transition={{ type: "spring", stiffness: 75 }}
             >
-                <div>
-                <div className="container">
-                    <div className="content">
-                        <h1>Blue</h1>
-                        <p>Hoodie taxidermy drinking vinegar, palo santo actually schlitz you probably haven't heard of them tofu kombucha keytar meggings. Adaptogen lomo brunch, activated charcoal austin ugh vape whatever schlitz semiotics tumblr.</p>
-                    </div>
-                    <div>
-                        <Link to='/'>
-                        <button className='btn btn-danger'>landing</button>
+                    <Stack>
+                <Flex direction={isNotSmallerScreen ? "row" : "column"}
+                    spacing="200px" p={isNotSmallerScreen ? "32" : "0"}
+                    alignSelf="flex-start">
+                        <Box mt={isNotSmallerScreen? "0" : "16"} align="flex-start">
+                            <Text fontSize='5xl' fontWeigth='semibold'>Texto de ejemplo</Text>
+                            <Text fontSize='7xl' fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip="text">Lorem ipsum dolor</Text>
+                            <Text fontSize='2xl'  color='gray.500'>sit amet porro meme ricca</Text>
+                            <Link to='/'>
+                        <button>landing</button>
                         </Link>
                         <Link to='/red'>
                         <button>red</button>
                         </Link>
-                    </div>
-                </div>
-</div>
+                        </Box>
+                </Flex>
+            </Stack>
             </motion.div>
-            
         )
     }
 export default Blue;
